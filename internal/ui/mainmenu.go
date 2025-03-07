b6abbe5972af7d1b6fa11df88d7d8a0458154c4a
Levi Lewis <levi@calmdev.com>
2025-03-06 21:19:50 -0500

    Add refresh to account menu.

--- internal/ui/mainmenu.go
+++ internal/ui/mainmenu.go
@@ -11,30 +11,43 @@ import (
 func MainMenu(w fyne.Window) *fyne.MainMenu {
 	return fyne.NewMainMenu(
 		fyne.NewMenu("Account",
-			fyne.NewMenuItem("Settings", func() {
-				w.SetContent(RenderSettingsView())
-				w.Show()
-			}),
-			fyne.NewMenuItem("Export Rewards", func() {
-				d := dialog.NewFileSave(
-					func(writer fyne.URIWriteCloser, err error) {
-						if err != nil {
-							return
-						}
-						if writer == nil {
-							return
-						}
-						defer writer.Close()
-						algo.ExportRewards(writer)
-					},
-					w,
-				)
-				d.SetFileName("rewards.csv")
-				d.SetFilter(storage.NewExtensionFileFilter([]string{".csv"}))
-				d.SetView(dialog.ListView)
-				d.Resize(fyne.NewSize(MainWindowWidth-100, MainWindowHeight-100))
-				d.Show()
-			}),
+			&fyne.MenuItem{
+				Label: "Refresh",
+				Action: func() {
+					w.SetContent(RenderMainView())
+					w.Show()
+				},
+			},
+			&fyne.MenuItem{
+				Label: "Settings",
+				Action: func() {
+					w.SetContent(RenderSettingsView())
+					w.Show()
+				},
+			},
+			&fyne.MenuItem{
+				Label: "Export Rewards",
+				Action: func() {
+					d := dialog.NewFileSave(
+						func(writer fyne.URIWriteCloser, err error) {
+							if err != nil {
+								return
+							}
+							if writer == nil {
+								return
+							}
+							defer writer.Close()
+							algo.ExportRewards(writer)
+						},
+						w,
+					)
+					d.SetFileName("rewards.csv")
+					d.SetFilter(storage.NewExtensionFileFilter([]string{".csv"}))
+					d.SetView(dialog.ListView)
+					d.Resize(fyne.NewSize(MainWindowWidth-100, MainWindowHeight-100))
+					d.Show()
+				},
+			},
 		),
 	)
 }
